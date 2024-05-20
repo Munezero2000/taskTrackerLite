8546478acc197cffe6f36f3048f15f53d442dfc0
Mag codes <muneagechaste@gmail.com>
2024-05-20 09:41:58 +0200

    added project template

---
+++ src/test/java/com/fad/tasktracker/TasktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fad.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
