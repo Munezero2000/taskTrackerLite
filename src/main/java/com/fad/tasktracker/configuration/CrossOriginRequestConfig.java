c0e8c218ac6ce5dc69d0c23f854ffdd68de66724
Mag codes <muneagechaste@gmail.com>
2024-05-23 14:36:14 +0200

    fixed cors

---
+++ src/main/java/com/fad/tasktracker/configuration/CrossOriginRequestConfig.java
@@ -0,0 +1,24 @@
+package com.fad.tasktracker.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+public class CrossOriginRequestConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); 
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*"); 
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
